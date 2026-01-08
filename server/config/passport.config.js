9d160322471f0bb0bedb605e090585292a729e6e
shanto <shantokumar22@gmail.com>
2026-01-09 00:04:11 +0600

    update

---
+++ server/config/passport.config.js
@@ -0,0 +1,26 @@
+// client side token er sathe server side user er information ta connect kore
+
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/user.model');
+const passport = require('passport');
+
+require('dotenv').config();
+
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET
+}
+
+passport.use(new JwtStrategy(opts, async function(jwt_payload, done) {
+    try {
+        const user = await User.findById(jwt_payload.id);
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    } catch (error) {
+        return done(error, false);
+    }
+}));
--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,19 +1,36 @@
+const userModel = require("../models/user.models");
+const register = async (req, res) => {
+  try {
+    // user check
+    const user = await userModel.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+    // user create
+    const newUser = new userModel({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    await newUser.save();
+    return res.status(201).json({
+      message: "User registered successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "User registration failed",
+    });
+  }
+};
 
-const register = (req, res) => {
-    
-}
+const login = (req, res) => {};
 
-
-const login = (req, res) => {
-    
-}
-
-const profile = (req, res) => {
-    
-}
+const profile = (req, res) => {};
 
 module.exports = {
-    register,
-    login,
-    profile
-}
+  register,
+  login,
+  profile,
+};
