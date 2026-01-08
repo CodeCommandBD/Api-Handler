c8671e6cac5a41f01b5c1005de4bd8fac58e8336
shanto <shantokumar22@gmail.com>
2026-01-09 01:12:15 +0600

    update

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,10 +1,14 @@
 const userModel = require("../models/user.models");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const register = async (req, res) => {
   try {
     // user check
     const user = await userModel.findOne({ email: req.body.email });
     if (user) {
-      return res.status(400).json({
+      return res.status(400).send({
         message: "User already exists",
       });
     }
@@ -15,17 +19,56 @@ const register = async (req, res) => {
       password: req.body.password,
     });
     await newUser.save();
-    return res.status(201).json({
+    return res.status(201).send({
+      status: true,
       message: "User registered successfully",
     });
   } catch (error) {
-    return res.status(500).json({
+    return res.status(500).send({
+      status: false,
       message: "User registration failed",
     });
   }
 };
 
-const login = (req, res) => {};
+const login = async (req, res) => {
+  try {
+    // user check... user already exists
+    const user = await userModel.findOne({email: req.body.email})
+    if(!user){
+      return res.status(400).send({
+        status: false,
+        message: "User not found"
+      })
+    }
+    
+    // compare password 
+    const isPasswordMatched = await user.comparePassword(req.body.password)
+
+    if(!isPasswordMatched){
+      return res.status(400).send({
+        status: false,
+        message: "Invalid Password"
+      })
+    }
+
+
+    // generate token 
+      const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "1h"});
+
+    // send response
+    return res.status(200).send({
+      status: true,
+      message: "User login successfully",
+      token: "Bearer " + token,  
+    })
+  } catch (error) {
+    return res.status(500).send({
+      status: false,
+      message: "User login failed",
+    })
+  }
+};
 
 const profile = (req, res) => {};
 
