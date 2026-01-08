bb226b36edce19fe16e6165659e8fdce550993dd
shanto <shantokumar22@gmail.com>
2026-01-09 02:25:45 +0600

    update

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
+const PORT = process.env.PORT || 3000;
 require("dotenv").config();
 const connectDB = require("./config/user.config");
-const PORT = process.env.PORT || 3000;
 
 // Connect to database first
 connectDB();
