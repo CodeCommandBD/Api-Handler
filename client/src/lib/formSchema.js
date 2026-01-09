9c1b9e0454c11b2c0986474d0c768a9513244c9c
shanto <shantokumar22@gmail.com>
2026-01-09 21:45:26 +0600

    update

---
+++ client/src/lib/formSchema.js
@@ -0,0 +1,12 @@
+import {z} from 'zod'
+
+export const registerSchema = z.object({
+    username: z.string().min(3, "কমপক্ষে ৩ অক্ষরের username দাও"),
+    email: z.string().email("সঠিক email দাও"),
+    password: z.string().min(6,"কমপক্ষে ৬ অক্ষরের password দাও")
+})
+
+export const loginSchema = z.object({
+  email: z.string().email("সঠিক email দাও"),
+  password: z.string().min(6, "কমপক্ষে ৬ অক্ষরের password দাও"),
+});
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,42 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Home from './Components/Home.jsx';
+import Login from './Components/Login.jsx';
+import Register from './Components/Register.jsx';
+import Profile from './Components/Profile.jsx';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children:[
+      {
+        path:'/',
+        element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      },
+      {
+        path: '/profile',
+        element: <Profile></Profile>
+      },
+
+    ]
+  },
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />,
+  <ToastContainer></ToastContainer>
 )
